1ae195853fb1e8f11539c16473a8443c2bd22575
daniels011 <dsergeyev22@gmail.com>
2024-05-07 11:04:52 -0400

    Add files via upload
    Added First Highway Image

---
+++ highway.py
@@ -0,0 +1,3 @@
+import pygame
+
+highway_base = pygame.image.load("Final Highway Base.png")
--- main.py
+++ main.py
@@ -1,2 +1,30 @@
-import random
 import pygame
+
+
+pygame.init()
+pygame.font.init()
+my_font = pygame.font.SysFont('Arial', 15)
+pygame.display.set_caption("Highway Racer")
+
+# set up variables for the display
+size = (1778, 1000)
+screen = pygame.display.set_mode(size)
+game_name = "Highway Racer"
+
+
+
+
+run = True
+
+# -------- Main Program Loop -----------
+while run:
+    # --- Main event loop
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            run = False
+
+    screen.fill((0, 0, 0))
+    pygame.display.update()
+
+
+pygame.quit()
