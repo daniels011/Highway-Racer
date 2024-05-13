2f92fcf2da5eaa2222a181de5dd8050452d7b593
daniels011 <dsergeyev22@gmail.com>
2024-05-13 11:03:07 -0400

    Made the Lanes Move

--- highway.py
+++ highway.py
@@ -2,11 +2,15 @@ import pygame
 
 class Highway:
 
-    def __init__(self, x, number_highway):
+    def __init__(self, x, y, number_highway):
         self.x = x
+        self.y = y
         self.number = number_highway
+        self.highway_image = pygame.image.load("highway" + str(number_highway) + ".png")
+        self.image_size = self.highway_image.get_size()
+        self.rect = pygame.Rect(self.x, self. y, self.image_size[0], self.image_size[1])
 
-    def
+    #def turn(self, x, key):
 
 
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-from highway import
+from highway import Highway
+import time
 
 pygame.init()
 pygame.font.init()
@@ -10,21 +11,37 @@ pygame.display.set_caption("Highway Racer")
 size = (1778, 1000)
 screen = pygame.display.set_mode(size)
 game_name = "Highway Racer"
+start_time = time.time()
+highway_number = 1
+h = Highway(0, 0, highway_number)
 
 
-
-
+frame = 0
 run = True
+clock = pygame.time.Clock()
 
 # -------- Main Program Loop -----------
 while run:
+    clock.tick(60)  #60 fps
     # --- Main event loop
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             run = False
+    time_elapsed = time.time() - start_time
+    if highway_number == 5:
+        highway_number = 1
+        print("hi")
+    if frame % 1 == 0:
+        h = Highway(0, 0, highway_number)
+        highway_number += 1
+        print(highway_number)
+
+
 
     screen.fill((0, 0, 0))
+    screen.blit(h.highway_image, h.rect)
     pygame.display.update()
 
+    frame += 1
 
 pygame.quit()
