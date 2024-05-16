209483b4a397a5f6e5ef17211e9ad5a83dcd6910
daniels011 <dsergeyev22@gmail.com>
2024-05-16 11:04:58 -0400

    Started Turning Logic

--- highway.py
+++ highway.py
@@ -7,10 +7,34 @@ class Highway:
         self.y = y
         self.number = number_highway
         self.highway_image = pygame.image.load("highway" + str(number_highway) + ".png")
+        self.turn_delta = 10
+        self.speed_delta = .2
         self.image_size = self.highway_image.get_size()
-        self.rect = pygame.Rect(self.x, self. y, self.image_size[0], self.image_size[1])
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
 
-    #def turn(self, x, key):
+    def rescale_image(self):
+        self.image_size = self.highway_image.get_size()
+        scale_size = (self.image_size[0] * 1.05, self.image_size[1] * 1.05)
+        self.highway_image = pygame.transform.scale(self.highway_image, scale_size)
+
+    def turn(self, direction):
+        if self.x <= 0 and self.x >= -240:
+            if self.x == 0:
+                if direction == "right":
+                    self.x += self.turn_delta
+            elif self.x == -250:
+                if direction == "left":
+                    self.x -= self.turn_delta
+            else:
+                if direction == "left":
+                    self.x -= self.turn_delta
+                if direction == "right":
+                    self.x += self.turn_delta
+
+
+    #def speed_change(
+
+
 
 
 
--- main.py
+++ main.py
@@ -13,7 +13,8 @@ screen = pygame.display.set_mode(size)
 game_name = "Highway Racer"
 start_time = time.time()
 highway_number = 1
-h = Highway(0, 0, highway_number)
+h = Highway(-20, 0, highway_number)
+h.rescale_image()
 
 
 frame = 0
@@ -27,16 +28,25 @@ while run:
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             run = False
+
+    keys = pygame.key.get_pressed()
+
+    #turning
+    if keys[pygame.K_d]:
+        h.turn("right")
+    if keys[pygame.K_a]:
+        h.turn("left")
+    print(h.x)
+
     time_elapsed = time.time() - start_time
     if highway_number == 5:
         highway_number = 1
-        print("hi")
     if frame % 1 == 0:
-        h = Highway(0, 0, highway_number)
+        h.rescale_image()
+        h = Highway(h.x, 0, highway_number)
         highway_number += 1
-        print(highway_number)
 
-
+    h.rescale_image()
 
     screen.fill((0, 0, 0))
     screen.blit(h.highway_image, h.rect)
