c59a1f4f467600f2c55df68b7922ca523b003a92
daniels011 <dsergeyev22@gmail.com>
2024-05-29 10:59:48 -0400

    Worked on the Banana Split

--- banana.py
+++ banana.py
@@ -2,15 +2,26 @@ import pygame
 
 class Banana:
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, size):
         self.x = x
         self.y = y
         self.banana_image = pygame.image.load("banana.png")
         self.image_size = self.banana_image.get_size()
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+        self.size = size
 
-    def rescale_image(self, size):
+    def rescale_image(self):
         self.image_size = self.banana_image.get_size()
-        scale_size = (self.image_size[0] * size, self.image_size[1] * size)
+        scale_size = (self.image_size[0] * self.size, self.image_size[1] * self.size)
         self.banana_image = pygame.transform.scale(self.banana_image, scale_size)
+
+    def move_banana(self):
+        self.x -= 1
+        if self.y < 500:
+            self.y += 1
+        self.size += 1
+
+
+
+
 
--- main.py
+++ main.py
@@ -19,15 +19,16 @@ h = Highway(-20, 0, highway_number)
 h.rescale_image()
 i = Interior(-50, 50)
 i.rescale_image()
-b = Banana(900, 600)
-banana_size_scale = .1
-b.rescale_image(banana_size_scale)
+b = Banana(900, 600, .1)
+b.rescale_image()
 speed = 30 #mph
-start = True
+start1 = True
 frame = 0
 run = True
 clock = pygame.time.Clock()
 s_image = pygame.image.load("startimage.jpg")
+
+
 # -------- Main Program Loop -----------
 while run:
     clock.tick(60)  #240 fps
@@ -43,7 +44,7 @@ while run:
         h.turn("right")
     if keys[pygame.K_a]:
         h.turn("left")
-    print(h.x)
+
 
 
     #accelarating
@@ -51,8 +52,10 @@ while run:
         speed += 5
     if keys[pygame.K_s]:
         speed -= 5
+    if keys[pygame.K_SPACE]:
+        start1 = False
     speed_change_divisor = speed / 5
-    print(speed_change_divisor)
+
     time_elapsed = time.time() - start_time
     if highway_number == 5:
         highway_number = 1
@@ -60,16 +63,17 @@ while run:
         h.rescale_image()
         h = Highway(h.x, 0, highway_number)
         highway_number += 1
-        b.x *= .1
+
     h.rescale_image()
     if frame % 5 == 0:
-        banana_size_scale += .2
-        b.rescale_image(banana_size_scale)
+        print(b.x)
+        b.x -= 5
+        b.rescale_image()
         b.y += 1
     if start1:
         welcome = welcome_font.render("Welcome To Highway Racer!", True, (255, 255, 255))
         screen.blit(welcome, (180, 500))
-        screen.blit(startimage)
+        screen.blit(s_image, (0, 0))
     else:
         screen.fill((0, 0, 0))
         screen.blit(h.highway_image, h.rect)
